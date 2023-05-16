d932c1e8e66d0bbe42e28c15a00d2ff581e2e8e2
EkkoG <beijiu572@gmail.com>
2023-05-17 00:05:19 +0800

    Refine log

--- src/subio/app/log.py
+++ src/subio/app/log.py
@@ -1,4 +1,20 @@
 import logging
 
-logger = logging.getLogger(__name__)
+class EmojiFormatter(logging.Formatter):
+    level_to_emoji = {
+        logging.DEBUG: '🐛',
+        logging.INFO: 'ℹ️',
+        logging.WARNING: '⚠️',
+        logging.ERROR: '❌',
+        logging.CRITICAL: '🚨',
+    }
+    def format(self, record):
+        record.levelname = f'{self.level_to_emoji[record.levelno]} {record.levelname}'
+        return super().format(record)
+
+logger = logging.getLogger('SubIO')
 logger.addHandler(logging.StreamHandler())
+
+# format log
+formatter = EmojiFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger.handlers[0].setFormatter(formatter)
