5516a0d9b91bcbcdd7484ef8096e6a4ded422d77
EkkoG <beijiu572@gmail.com>
2023-05-19 00:34:34 +0800

    Remove unit of hysteria up and down

--- src/subio/app/parser/clash.py
+++ src/subio/app/parser/clash.py
@@ -1,4 +1,5 @@
 import yaml
+import re
 
 from .common import common_transform_list
 
@@ -7,4 +8,11 @@ def parse(config, sub_text):
     return nodes
 
 def transform_list(lst, unify_map):
-    return common_transform_list(lst, unify_map)
+    common_trans = common_transform_list(lst, unify_map)
+    def fix(node):
+        if node['type'] == 'hysteria':
+            # only keep numbers of up and down, remove unit
+            node['up'] = int(re.sub(r'\D', '', node['up']))
+            node['down'] = int(re.sub(r'\D', '', node['down']))
+        return node
+    return list(map(fix, common_trans))
